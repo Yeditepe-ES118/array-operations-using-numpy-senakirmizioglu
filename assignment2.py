cd2a3cdc95d590a1d774e91f57fe14a582ae2806
senakirmizioglu <sena.kirmizioglu@std.yeditepe.edu.tr>
2026-01-03 16:46:57 +0300

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,11 @@
+import numpy as np
+def stat():
+    data = np.loadtxt('populations.txt')
+    hare = data[:, 1]
+    min_hare_idx = np.argmin(hare)
+    min_year_hare = data[min_hare_idx, 0]
+    lynx_avg = np.mean(data[:, 2])
+    species_sum = np.sum(data[:, 1:], axis=1)
+    new_data = np.column_stack((data, species_sum))
+    new_data[new_data[:, 3] < 40000, 3] = 0
+    return data, hare, min_year_hare, lynx_avg, new_data
